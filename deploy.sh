71784c62863f35b28f983dc904ad59038706ca33
benubah <ben@rpowerlabs.org>
2020-02-14 22:03:49 +0100

    clean up line breaks

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 #set -o errexit -o nounset
 #BASE_REPO=$PWD
-
 setup_git() {
 Rscript -e 'source("R/pyladies.R")'
 Rscript -e 'source("R/pyladies_hosts.R")'
-
-  git config --global user.name "benubah"
-  git config --global user.email "ben@rpowerlabs.org"
+git config --global user.name "benubah"
+git config --global user.email "ben@rpowerlabs.org"
 }
-
 commit_files() {
   git checkout master
   # Current month and year, e.g: May 2019
@@ -20,13 +17,11 @@ commit_files() {
   git add -f docs/data/pyladies_summary.json
   git add -f docs/data/pyladies_cumulative.json
   git add -f docs/data/pyladies_map_data.geojson
-  
   # Create a new commit with a custom build message
   # with "[skip ci]" to avoid a build loop
   # and Travis build number for reference
   git commit -m "Travis Cron Jobs Update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
 }
-
 upload_files() {
   # Remove existing "origin"
   git remote rm origin
@@ -34,9 +29,6 @@ upload_files() {
   git remote add origin https://${GITTOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
   git push origin master --quiet
 }
-
 setup_git
-
 commit_files
-
 upload_files
